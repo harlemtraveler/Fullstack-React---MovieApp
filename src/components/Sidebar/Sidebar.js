b83cb46a38799f9947f1485d1e72068417de1cf2
Timothy Keaveny <timothykeaveny@Timothys-MacBook-Pro.local>
2018-07-08 14:54:43 -0400

    Pre-creation of two directories for client application & backend..."Save-point"

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const isActive = (match, location) => {
+    return match && match.path === location.pathname
+}
 
 export const Sidebar = props => (
     <div className="sidebar">
-        Sidebar goes here
+        <section>
+            <div className='heading'>Nav</div>
+        </section>
+        <section>
+            <div className='heading'>Links</div>
+            <ul>
+                <li>
+                    <NavLink
+                        to='/' isActive={isActive}
+                    >Home</NavLink></li>
+                <li>
+                    <NavLink
+                        to='/about' isActive={isActive}
+                    >About</NavLink></li>
+            </ul>
+        </section>
     </div>
 )
 
---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    Route
+} from 'react-router-dom';
+import {
+    pageWithoutLayout
+} from './containers/page';
+
+import Home from './views/Home/Home';
+import About from './views/About/About';
+
+export const Routes = props => (
+    <div className='app'>
+        <Route
+            path='/'
+            exact
+            component={Home}
+        />
+        <Route
+            path='/about'
+            render={(renderProps) => (
+                <About
+                    {...props}
+                    {...renderProps}
+                />
+            )}
+        />
+    </div>
+)
+
+export default pageWithoutLayout(Routes);
